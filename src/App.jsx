72722d79cfbfdb8d2334aeff8d10b9949bfcf093
Bavic <bavic97@gmail.com>
2025-04-16 09:35:19 +0100

    route

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import {
   About,
   Contact,
-  // Experience,
-  // Feedbacks,
   Hero,
   Navbar,
-  Tech,
   Works,
-  StarsCanvas,
   Footer,
-  // Home,
 } from "./components";
 
 const App = () => {
   return (
-    // <BrowserRouter>
-    //   <div className="relative z-0 bg-primary">
-    //     <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat">
-    //       <Navbar />
-    //       <Hero />
-    //     </div>
-    //     <About />
-    //     <Experience />
-    //     <Tech />
-    //     <Works />
-    //     <Feedbacks />
-    //     <div className="relative z-0">
-    //       <Contact />
-    //       <StarsCanvas />
-    //     </div>
-    //   </div>
-    // </BrowserRouter>
-
     <BrowserRouter>
       <div className='min-h-screen flex flex-col justify-between'>
         <Navbar />
